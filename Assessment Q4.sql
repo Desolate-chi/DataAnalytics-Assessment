89af9fc2a0304a2173998ef09d82edb50574367a
Desolate-chi <105279126+Desolate-chi@users.noreply.github.com>
2025-05-18 23:38:25 +0100

    Update Assessment Q4.sql

--- Assessment Q4.sql
+++ Assessment Q4.sql
@@ -3,8 +3,8 @@ SELECT u.id AS customer_id,CONCAT(u.first_name," ", u.last_name) AS name,
     TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_in_months,
     COUNT(s.transaction_reference) AS total_transactions,
     ROUND((COUNT(s.transaction_reference) / 
-        NULLIF(TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()), 0)) * 12 
-        * (SUM(s.confirmed_amount) * 0.001 / COUNT(s.transaction_reference)), 2
+    NULLIF(TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()), 0)) * 12 
+    * (SUM(s.confirmed_amount) * 0.001 / COUNT(s.transaction_reference)), 2
     ) AS estimated_clv
 FROM users_customuser AS u
 JOIN savings_savingsaccount AS s
